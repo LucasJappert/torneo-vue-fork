056f8497fa1041cf000417da2b44d3ae07633992
JulianaTolosa <julianatolosa4@gmail.com>
2022-06-03 20:12:49 -0300

    arrancar proyecto

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="containerMenu">
+      <div class="itemMenu">Información</div>
+      <div class="itemMenu">Información</div>
+  </div>
+</template>
+
+<script>
+//TODO: hacer mas tarde
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.containerMenu{
+  position:relative;
+  height:30px;
+  width:100%;
+  display:flex;
+  align-items: center;
+  justify-content: left;
+}
+</style>
