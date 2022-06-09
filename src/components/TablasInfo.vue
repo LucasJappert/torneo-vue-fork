3349ddc1a4068b0f586b888b523b4a48e6553af7
JulianaTolosa <julianatolosa4@gmail.com>
2022-06-09 19:30:29 -0300

    tablas

--- src/components/TablasInfo.vue
+++ src/components/TablasInfo.vue
@@ -28,6 +28,36 @@
             
             
         </div>
+        <div class="divTableRow">
+            <div class="divTableCell">2013</div>
+            <div class="divTableCell"><input type="checkbox" disabled/></div>
+            
+            
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">2014</div>
+            <div class="divTableCell"><input type="checkbox" disabled/></div>
+            
+            
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">2015</div>
+            <div class="divTableCell"><input type="checkbox" disabled/></div>
+            
+            
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">2016</div>
+            <div class="divTableCell"><input type="checkbox" disabled/></div>
+            
+            
+        </div>
+        <div class="divTableRow">
+            <div class="divTableCell">2017</div>
+            <div class="divTableCell"><input type="checkbox" disabled/></div>
+            
+            
+        </div>
         </div>
         
         
@@ -42,18 +72,21 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style ks>
 
 .divTable{
 	display: table;
 	width: 50%;
     padding-left: 450px;
+    cursor: pointer;
 }
 .divTableRow {
 	display: table-row;
     
 }
-
+ .divTableRow:nth-child(2n+2){
+    background: #aacae5ed;
+}
 .divTableCell, .divTableHead {
 	border: 1px solid #fff(214, 38, 38);
 	display: table-cell;
@@ -64,7 +97,7 @@ export default {
 	display: table-header-group;
 	font-weight: bold;
     color: white;
-
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
         
 }
 .divTableFoot {
@@ -74,16 +107,21 @@ export default {
 }
 .divTableBody {
 	display: table-row-group;
+    
     & .divTableCell{
-        border-bottom: 1px solid black
+        border-bottom: 1px solid #5c9fd6ed
+        
     }
     & .divTableRow {
 	
         border-bottom: 1px solid black;
+        
 
         &:hover{
-                background-color: rgb(165, 165, 165);
+                background-color: rgb(190, 189, 189);
             }
+        
+        
 }
 }
 
