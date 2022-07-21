bd18c73d5dcf57d8f44381b5a8fdc17e620daaa9
Lucas Jappert <lucasnicolasjappert@gmail.com>
2022-07-21 19:38:58 -0300

    arreglar padding en body

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,6 @@ export default {
 body{
   margin:0;
 }
-.ViewContainer{
-  padding:0 5px;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -69,7 +66,7 @@ body{
     width: 100%;
   overflow: hidden;
   min-height: 300px;
-
+  min-width: 1000px;
 }
 .ContenedorImagenFondo{
   position:fixed;
