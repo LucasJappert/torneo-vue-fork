cf9ec793e292d9217663d8b76714dc33028a11d8
JulianaTolosa <julianatolosa4@gmail.com>
2023-09-16 19:38:52 -0300

    interzonal

--- src/models/InfoEquipo.js
+++ src/models/InfoEquipo.js
@@ -10,16 +10,16 @@ const InfoEquipo = function (nombre){
     this.Ptos = 0;
 
     this.setData = (puntosGanados, golesAFavor, golesEnContra) => {
-        this.PJ += 1;
+        if (!this.Nombre.includes("INTERZONAL")) this.PJ += 1;
         if(puntosGanados == 3)
-            this.PG += 1;
+            if (!this.Nombre.includes("INTERZONAL")) this.PG += 1;
         if(puntosGanados == 1)
-            this.PE += 1;
+            if (!this.Nombre.includes("INTERZONAL")) this.PE += 1;
         if(puntosGanados == 0)
-            this.PP += 1;
-        this.GF += Number(golesAFavor);
-        this.GC += Number(golesEnContra);
-        this.DG = this.GF - this.GC;
+            if (!this.Nombre.includes("INTERZONAL")) this.PP += 1;
+        if (!this.Nombre.includes("INTERZONAL")) this.GF += Number(golesAFavor);
+        if (!this.Nombre.includes("INTERZONAL")) this.GC += Number(golesEnContra);
+        if (!this.Nombre.includes("INTERZONAL")) this.DG = this.GF - this.GC;
 
         if (!this.Nombre.includes("INTERZONAL")) this.Ptos += puntosGanados;
     }
